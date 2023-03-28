0aed228d6b5ba21d3708f3ec6027932f57ee2792
rkrobby19 <rizkyrobby19@gmail.com>
2023-03-28 09:07:40 +0700

    Initial file config

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const URI = process.env.MONGO_URI;
+const PORT = process.env.PORT || 8000;
+
+mongoose
+  .connect(URI)
+  .then(() =>
+    app.listen(PORT, () => {
+      console.log(`Server Running on Port: http://localhost:${PORT}`);
+    })
+  )
+  .catch((err) => console.log(err));
